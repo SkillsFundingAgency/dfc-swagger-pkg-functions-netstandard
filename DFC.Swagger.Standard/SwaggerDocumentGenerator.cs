af2332ddeeb1222a3fedb6f233999b32b49f8211
Luke Weller <33352024+myselfweller@users.noreply.github.com>
2022-03-28 09:22:30 +0100

    Make subcontractor mandatory

--- DFC.Swagger.Standard/SwaggerDocumentGenerator.cs
+++ DFC.Swagger.Standard/SwaggerDocumentGenerator.cs
@@ -271,7 +271,8 @@ namespace DFC.Swagger.Standard
                 opHeaderParam2.name = "SubcontractorId";
                 opHeaderParam2.description = "The subcontractor id";
                 opHeaderParam2.@in = "header";
-                opHeaderParam2.required = false;
+                opHeaderParam2.required = true;
+
                 opHeaderParam2.type = "string";
                 parameterSignatures.Add(opHeaderParam2);
             }
