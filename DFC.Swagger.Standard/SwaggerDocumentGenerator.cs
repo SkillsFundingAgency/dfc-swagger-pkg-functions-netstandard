5a198ad5bbc461b794fdd2e21106a2a44dd8d06c
Joe Hudson-Small <Joehs@me.com>
2022-08-25 13:35:10 +0100

    re-add conditional documentation change for mandatory subcontractorid

--- DFC.Swagger.Standard/SwaggerDocumentGenerator.cs
+++ DFC.Swagger.Standard/SwaggerDocumentGenerator.cs
@@ -250,7 +250,7 @@ namespace DFC.Swagger.Standard
             return responses;
         }
 
-        private List<object> GenerateFunctionParametersSignature(MethodInfo methodInfo, string route, dynamic doc)
+        private List<object> GenerateFunctionParametersSignature(MethodInfo methodInfo, string route, dynamic doc, string funcName)
         {
             var parameterSignatures = new List<object>();
 
@@ -271,9 +271,13 @@ namespace DFC.Swagger.Standard
                 opHeaderParam2.name = "SubcontractorId";
                 opHeaderParam2.description = "The subcontractor id";
                 opHeaderParam2.@in = "header";
-                opHeaderParam2.required = true;
+                opHeaderParam2.required = false;
                 opHeaderParam2.type = "string";
                 parameterSignatures.Add(opHeaderParam2);
+                if (funcName.ToLower().Contains("outcomes"))
+                { 
+                    opHeaderParam2.Required = true; 
+                }
             }
 
             var postBodyAttr = methodInfo.GetCustomAttributes().FirstOrDefault(attr => attr is PostRequestBodyAttribute);
