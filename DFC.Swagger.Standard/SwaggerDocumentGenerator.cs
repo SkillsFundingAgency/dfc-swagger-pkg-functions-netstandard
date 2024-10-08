596bb4f36848ebc68a049560f2ef781460e45071
hnandra <h_nandra@hotmail.co.uk>
2024-10-08 16:07:00 +0100

    Fixed English in parameter type again

--- DFC.Swagger.Standard/SwaggerDocumentGenerator.cs
+++ DFC.Swagger.Standard/SwaggerDocumentGenerator.cs
@@ -535,7 +535,7 @@ namespace DFC.Swagger.Standard
 
                 if (paramType == "System.Nullable`1[NCS.DSS.Contact.ReferenceData.PreferredContactMethod]")
                 {
-                    opParam.type = "integer or string or enum";
+                    opParam.type = "integer, string or enum";
                 }
 
                 var enumValues = new List<string>();
